bc60962ad892a39c7deb1d83d40d2cec82d93d20
pinkas <benoit.pinkasfeld@gmail.com>
2016-07-06 18:47:14 +1000

    SlugPhysics now supports horizontal detection and maximum slope

--- Assets/Scripts/SlugPhysics.cs
+++ Assets/Scripts/SlugPhysics.cs
@@ -18,10 +18,12 @@ public class SlugPhysics : MonoBehaviour {
     public float airHighVelocityMovementFactor = 1.4f;
     private const float rayCastRestLength = 0.03f;
     private float movementFactor = 1.1f; // TODO movementFactor at init is the same as groundMovementFactor
+    private const float maxSlope = 0.8f;
+
 
     private Vector2 absoluteVelocity;
 
-    private RaycastHit2D underMyFeetHit;
+    private RaycastHit2D[] rayCastHit = new RaycastHit2D[1];
     private RaycastHit2D inFrontOfMe;
 
     private bool inTheAir = false;
@@ -51,163 +53,111 @@ public class SlugPhysics : MonoBehaviour {
     }
 
     void FixedUpdate() {
-
+        // 1 - Update velocities
         CalculateVelocity();
-
-        RaycastHit2D underMe = WhatIsUnderMyFeetRightNow();
-        Vector2 currentSlope = GetSlopeFromRayCastHid2D(underMe);
-
-        float dx, dy;
-        if (InTheAir) {
-            dx = absoluteVelocity.x * movementFactor * Time.fixedDeltaTime;
-            dy = absoluteVelocity.y * Time.fixedDeltaTime;
-            myPrint("inTheAir yo");
+        
+        // 2 - Calculate translations depending on current current slope 
+        Vector2 groundSlope;
+        if (WhatIsUnderMyFeet(Vector2.zero) > 0) {
+            groundSlope = GetSlopeFromRayCastHid2D(rayCastHit[0]);
         } else {
-            dx = absoluteVelocity.x * movementFactor * Time.fixedDeltaTime * Mathf.Abs(currentSlope.x);
-            dy = Mathf.Abs(absoluteVelocity.x) * movementFactor * Time.fixedDeltaTime * currentSlope.y;
-            myPrint("on the ground yo " + dx +"/"+dy);
+            groundSlope = Vector2.zero;
         }
+        Vector2 transCandidate = CalculateTranslation(groundSlope);
+        //Vector2 end = new Vector2(boxCollider.bounds.center.x, boxCollider.bounds.min.y);
 
-        Vector2 transCandidate = new Vector2(dx, dy);
-
-        RaycastHit2D inFront = WhatIsInFrontOfMe(transCandidate);
-        Vector2 facingWallslope = GetSlopeFromRayCastHid2D(inFront);
-        if (inFront.collider != null && Mathf.Abs(facingWallslope.y) > 0.85f) {
-            transCandidate.x = 0;
-            dx = 0;
-        }
-
-        RaycastHit2D underMeFutur = WhatIsUnderMyFeet(transCandidate);
-        if (underMeFutur.collider != null) {
-            myPrint(" collision detection " + inTheAir + " " + absoluteVelocity.y);
-            if (inTheAir && absoluteVelocity.y > 0) {
-                // rien
-            }else if (inTheAir && absoluteVelocity.y < 0) {
-                StopFalling();
-                dy = FixYTrans(underMeFutur);
-            } else {
-                myPrint("On THE GROUND");
-                dy = FixYTrans(underMeFutur);
-            }
-        } else if (underMeFutur.collider == null && !inTheAir) {
-            StartFalling();
-            myPrint("start falling");
-        }
-
-        transform.Translate(dx, dy, 0, Space.World);
-    }
-
-    float calculateXTranslation() {
-        //float xTranslation = absoluteVelocity.x * movementFactor * Time.fixedDeltaTime;
-        //float penteX = 1 - Mathf.Abs(penteY);
-        float penteX = Mathf.Abs(groundSlope.x);
-        if (InTheAir) {
-            xTranslation = absoluteVelocity.x * movementFactor * Time.fixedDeltaTime;
-        } else {
-            xTranslation = penteX * absoluteVelocity.x * movementFactor * Time.fixedDeltaTime;
-        }
-
-        if (groundSlope.y > 0.85f) {
-                xTranslation = 0;
-                print(" YO");
-        }
-
-            //print("Xtranslation " + penteX);
-        return xTranslation;
-    }
-
-    float calculateYTranslation() {
-        float yTranslation = 0;
-        bool justFellFromPlatform = underMyFeetHit.collider == null
-                && inFrontOfMe.collider == null
-                && !inTheAir;
-        bool justLanding = underMyFeetHit.collider != null && inTheAir && absoluteVelocity.y < 0;
-        bool justBouncing = justLanding && bounceFactor > 0;
-
-        if (justFellFromPlatform) {
-            StartFalling();
-        } else if (justBouncing) {
-            yTranslation = 0;
-            absoluteVelocity.y = initialJumpVelocity * bounceFactor;
-            //            absoluteVelocity.x = bounceFactor*5;
-            NotifyObservers(SlugEvents.HitGround);
-        } else if (justLanding) {
-            StopFalling();
-            yTranslation = FixYTrans(underMyFeetHit);
-        } else if (inTheAir) {
-            yTranslation = absoluteVelocity.y * Time.fixedDeltaTime;
-        } else {
-            // I may be going up/down a hill
-            //yTranslation = Mathf.Abs(absoluteVelocity.x) * movementFactor * Time.fixedDeltaTime * penteY;
-            if (groundSlope.y > 0.85f) {
-                yTranslation = 0;
-                print("YEQQQQQQQQQQQQQQQQQQQQQQH");
-            } else {
-                yTranslation = Mathf.Abs(absoluteVelocity.x) * movementFactor * Time.fixedDeltaTime * groundSlope.y + FixYTrans(underMyFeetHit);
-                //yTranslation = Mathf.Abs(absoluteVelocity.x) * movementFactor * Time.fixedDeltaTime * groundSlope.y ;
+        // 3 - Adjust translations if collisions next frame
+        // 3.1 - Adjust x
+        if (WhatIsInFrontOfMe(transCandidate) > 0) {
+            Vector2 facingWallslope = GetSlopeFromRayCastHid2D(rayCastHit[0]);
+            if (Mathf.Abs(facingWallslope.y) > maxSlope) {
+                transCandidate.x = FixXTrans(rayCastHit[0]);
+                if (!inTheAir) {
+                    transCandidate = Vector2.zero;
+                    goto TRANSLATE;
+                }
             }
         }
-        return yTranslation;
+        // 3.2 Adjust y
+        if (WhatIsUnderMyFeet(transCandidate) > 0) {
+            Vector2 futurUnderslope = GetSlopeFromRayCastHid2D(rayCastHit[0]);
+            if (inTheAir) {
+                if (absoluteVelocity.y < 0) { // landing
+                    if (Mathf.Abs(futurUnderslope.y) < maxSlope) {
+                        StopFalling();
+                    }
+                    transCandidate.y = FixYTrans(rayCastHit[0]);
+                }
+            } else {
+                transCandidate.y = FixYTrans(rayCastHit[0]); // just following the curve of the ground
+            }
+        } else if (!inTheAir) {
+            StartFalling();
+        }
+
+        TRANSLATE:
+        transform.Translate(transCandidate.x, transCandidate.y, 0, Space.World);
     }
 
-    RaycastHit2D  WhatIsUnderMyFeet(Vector2 trans) {
-        float WHAT_NAME = 0.05f;
-        Vector2 startPoint = new Vector2(boxCollider.bounds.center.x + trans.x, boxCollider.bounds.min.y + trans.y - WHAT_NAME);
-        Vector2 endPoint = new Vector2(startPoint.x, startPoint.y + WHAT_NAME*2);
 
-        RaycastHit2D under = Physics2D.Linecast(endPoint, startPoint, linecastLayerMask);
-        myPrint("futur " + under.collider);
-        Debug.DrawLine(endPoint, startPoint);
-        return under;
+    Vector2 CalculateTranslation(Vector2 groundSlope) {
+        Vector2 trans = new Vector2();
+        if (inTheAir && groundSlope != Vector2.zero && absoluteVelocity.y < 0) { // sliding on a steep slope
+            trans.x = 1.8f * Time.fixedDeltaTime * Mathf.Abs(groundSlope.x);
+            trans.y = -1.8f * Time.fixedDeltaTime * Mathf.Abs(groundSlope.y) ;
+        } else if (InTheAir) { // falling or ascending
+            trans.x = absoluteVelocity.x * movementFactor * Time.fixedDeltaTime;
+            trans.y = absoluteVelocity.y * Time.fixedDeltaTime;
+        } else { // On the ground
+            trans.x = absoluteVelocity.x * movementFactor * Time.fixedDeltaTime * Mathf.Abs(groundSlope.x);
+            trans.y = Mathf.Abs(absoluteVelocity.x) * movementFactor * Time.fixedDeltaTime * groundSlope.y;
+        }
+        return trans;
     }
 
-    RaycastHit2D WhatIsInFrontOfMe(Vector2 trans) {
+
+    int WhatIsUnderMyFeet(Vector2 trans) {
+        Vector2 endPoint = new Vector2(boxCollider.bounds.center.x + trans.x, boxCollider.bounds.min.y + trans.y - 0.006f);
+        Vector2 startPoint = new Vector2(endPoint.x, boxCollider.bounds.min.y + 0.05f);
+        //Only supporting one hit per cast, no need for more for now
+        int hitCount = Physics2D.LinecastNonAlloc(startPoint, endPoint, rayCastHit, linecastLayerMask);
+        //Debug.DrawLine(startPoint, endPoint);
+        return hitCount;
+    }
+
+    int WhatIsInFrontOfMe(Vector2 trans) {
         Bounds bounds = boxCollider.bounds;
-        float startX = bounds.center.x + transform.right.x * bounds.size.x / 2;
+        float startX = bounds.center.x;
 
-        float safety = 0.02f * transform.right.x;
-        if (Mathf.Abs(trans.x) > safety) {
-            safety = 0;
+        Vector2 startPoint = new Vector2(startX, bounds.min.y);
+        Vector2 endPoint = startPoint + new Vector2(trans.x, trans.y);
+
+        int hitCount = Physics2D.LinecastNonAlloc(startPoint, endPoint, rayCastHit, linecastLayerMask);
+        Debug.DrawLine(startPoint, endPoint);
+        if (hitCount == 0) {
+            startPoint = new Vector2(startX, bounds.max.y);
+            endPoint = startPoint + new Vector2(trans.x, trans.y);
+            hitCount = Physics2D.LinecastNonAlloc(startPoint, endPoint, rayCastHit, linecastLayerMask);
         }
-        safety = 0;
-
-        Vector2 startPoint = new Vector2(startX, bounds.max.y);
-        Vector2 endPoint = startPoint + new Vector2(trans.x + safety, trans.y);
-
-        inFrontOfMe = Physics2D.Linecast(startPoint, endPoint, linecastLayerMask);
-
-        if (inFrontOfMe.collider == null) {
-            startPoint = new Vector2(startX, bounds.min.y);
-            endPoint = startPoint + new Vector2(trans.x+safety, trans.y);
-            inFrontOfMe = Physics2D.Linecast(startPoint, endPoint, linecastLayerMask);
-        }
-        return inFrontOfMe;
+        return hitCount;
     }
 
-    RaycastHit2D WhatIsUnderMyFeetRightNow() {
-        float dy = -0.2f;
-        rayCastStartPoint = new Vector2(boxCollider.bounds.center.x, boxCollider.bounds.min.y + 0.1f);
-        Vector2 endPoint = new Vector2(rayCastStartPoint.x, rayCastStartPoint.y + dy);
-
-        return Physics2D.Linecast(rayCastStartPoint, endPoint, linecastLayerMask);
+    float FixXTrans(RaycastHit2D hit) {
+        if (transform.right == Vector3.left) {
+            return rayCastHit[0].point.x - boxCollider.bounds.center.x + 0.03f;
+        } else {
+            return rayCastHit[0].point.x - boxCollider.bounds.center.x - 0.03f;
+        }
     }
 
     float FixYTrans(RaycastHit2D hit) {
         return hit.point.y - boxCollider.bounds.min.y + 0.005f;
-
     }
 
     Vector2 GetSlopeFromRayCastHid2D(RaycastHit2D hit) {
         Quaternion rotate = Quaternion.Euler(0, 0, -90 * transform.right.x);
-
         Vector2 slope = rotate * hit.normal;
-        slope.Normalize(); //Is it really necessary to normalize
         return slope;
-/*
-        Debug.DrawLine(underMyFeetHit.point,
-                        new Vector2(underMyFeetHit.point.x + groundSlope.x,  underMyFeetHit.point.y + groundSlope.y),
-                        Color.cyan);
-*/
     }
 
     void StopFalling() {
@@ -231,8 +181,6 @@ public class SlugPhysics : MonoBehaviour {
         } else {
             absoluteVelocity.x =  absoluteVelocity.x * groundDrag + forceX;
         }
-
-            print("abs Vel:" + " " + absoluteVelocity.x);
     }
 
     public bool JumpLowVel() {
