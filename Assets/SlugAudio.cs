baade129aa4008574d33c5f42baffb7b69b965ea
pinkas <benoit.pinkasfeld@gmail.com>
2016-09-01 20:18:16 +1000

    add sounds assets and 2 new classes 2 deal with sound SlugAudioManager

---
+++ Assets/SlugAudio.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Slug {
+
+    public class SlugAudio : MonoBehaviour {
+
+        private AudioSource audioSource;
+        private bool soundHasStarted;
+
+        void Awake() {
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        void Update() {
+            if (!audioSource.isPlaying) {
+               gameObject.SetActive(false);
+            }
+        }
+
+
+    }
+}
---
+++ Assets/SlugAudioManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class SlugAudioManager : MonoBehaviour {
+
+    private ObjectPoolScript audioSourcePool;
+    public AudioClip[] audioClips;
+
+    private AudioSource source;
+
+    void Awake() {
+        audioSourcePool = GetComponent<ObjectPoolScript>();
+    }
+
+    public void PlaySound(int soundIndex) {
+        GameObject audioGameObject = audioSourcePool.GetPooledObject();
+        AudioSource audioSource = audioGameObject.GetComponent<AudioSource>();
+        audioSource.clip = audioClips[soundIndex];
+        audioSource.Play();
+    }
+
+    public void PlaySound(AudioClip clip) {
+        GameObject audioGameObject = audioSourcePool.GetPooledObject();
+        AudioSource audioSource = audioGameObject.GetComponent<AudioSource>();
+        audioSource.clip = clip;
+        audioSource.Play();
+    }
+
+    public void PlaySoundSameSource(int soundIndex) {
+        if (source == null) {
+            GameObject audioGameObject = audioSourcePool.GetPooledObject();
+            source = audioGameObject.GetComponent<AudioSource>();
+            source.clip = audioClips[soundIndex];
+        }
+        source.Play();
+    }
+
+    //FIXME
+    void Update() {
+        if (source != null && !source.isPlaying) {
+            source = null;
+        }
+    }
+
+
+}
