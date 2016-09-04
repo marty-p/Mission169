19846635d56ab96f945c24977a095e9609818608
pinkas <benoit.pinkasfeld@gmail.com>
2016-09-04 17:40:27 +1000

    add knifeAnimationEvent

---
+++ Assets/KnifeAnimationEvent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class KnifeAnimationEvent : MonoBehaviour {
+
+    private Animator topAnimator;
+
+    void Awake() {
+        topAnimator = GetComponent<Animator>();
+    }
+
+    public void AEEndOfKnifeAnim() {
+        topAnimator.SetBool("knifeing", false);
+    } 
+
+    public void AEStartOfKnifeAnim() {
+        topAnimator.SetBool("knifeing", true);
+    }
+
+}
