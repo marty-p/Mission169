c8d3b56c1bdf16ef58e7f1173fdc93eeba345818
pinkas <benoit.pinkasfeld@gmail.com>
2016-09-04 17:37:45 +1000

    add goRight.cs (ommited in another commit)

---
+++ Assets/GoRight.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class GoRight : MonoBehaviour {
+
+    private AudioSource audio;
+
+    void Awake() {
+        print("awake");
+        audio = GetComponent<AudioSource>();
+        EventManager.StartListening("all_waves_over",
+                () => { gameObject.SetActive(true); });
+        gameObject.SetActive(false);
+	}
+	
+ void Start() {
+        print("Start");
+	}
+
+
+    public void PlaySound() {
+        audio.Play();
+    }
+
+    public void SetInactive() {
+        gameObject.SetActive(false);
+    }
+}
