b417bd95d7fdf604ac31f2ef83eab9ec2f469bb7
pinkas <benoit.pinkasfeld@gmail.com>
2016-09-11 18:10:07 +1000

    remove some prints

--- Assets/GoRight.cs
+++ Assets/GoRight.cs
@@ -2,21 +2,15 @@
 
 public class GoRight : MonoBehaviour {
 
-    private AudioSource audio;
+    private new AudioSource audio;
 
     void Awake() {
-        print("awake");
         audio = GetComponent<AudioSource>();
         EventManager.StartListening("all_waves_over",
                 () => { gameObject.SetActive(true); });
         gameObject.SetActive(false);
 	}
 	
- void Start() {
-        print("Start");
-	}
-
-
     public void PlaySound() {
         audio.Play();
     }
