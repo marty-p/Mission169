03cc63e5486f4f8e1da65ee47df2e72848ad2d63
pinkas <benoit.pinkasfeld@gmail.com>
2016-09-11 17:56:06 +1000

    enemy spawner can now spawn a single enemy too (not just an enenmy from a pool)

--- Assets/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -3,20 +3,34 @@
 public class EnemySpawner : MonoBehaviour {
 
     public ObjectPoolScript enemyPool;
-    private new BoxCollider2D collider;
+    public GameObject singleEnemyPrefab;
     private GameObject enemy;
+    private new BoxCollider2D collider;
 
     public bool EnemyAlive() {
         return enemy.active;
     }
+    public GameObject GetEnemy() {
+        return enemy;
+    }
 
     void Awake () {
         collider = GetComponent<BoxCollider2D>();
+        if (singleEnemyPrefab) {
+            enemy = Instantiate(singleEnemyPrefab);
+            enemy.SetActive(false);
+        }
 	}
 
     void InitEnemy() {
-        enemy = enemyPool.GetPooledObject();
-        enemy.transform.position = transform.position;
+        if (singleEnemyPrefab != null) {
+            enemy.SetActive(true);
+        } else if (enemyPool != null) {
+            enemy = enemyPool.GetPooledObject();
+            enemy.transform.position = transform.position;
+        } else {
+            print("Enemy Spawner, no single enemy or pool passed");
+        }
     }
 
     void OnTriggerEnter2D(Collider2D col) {
