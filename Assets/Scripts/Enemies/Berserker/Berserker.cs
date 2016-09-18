fa615fa4f621ac73dce2fc618b63a2c6ff15dcd9
pinkas <benoit.pinkasfeld@gmail.com>
2016-09-18 19:43:55 +1000

    add Berserker

---
+++ Assets/Scripts/Enemies/Berserker/Berserker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+
+public class Berserker : MonoBehaviour, IReceiveDamage {
+
+    public GameObject topBody;
+    public GameObject BottomBody;
+    public Animator BottomBodyAnimator;
+    private SlugPhysics physic;
+    private AreaOfEffectProjectile knife;
+
+    void Awake() {
+        physic = GetComponent<SlugPhysics>();
+        knife = GetComponent<AreaOfEffectProjectile>();
+    }
+
+    public void OnEnable() {
+    }
+
+    public void FixedUpdate() {
+        physic.MoveForward();
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "Player") {
+            Attack();
+        }
+    }
+
+    private void Attack() {
+        topBody.SetActive(true);
+        BottomBodyAnimator.SetBool("attack", true);
+        knife.CastAOE("Player", transform.position); 
+    }
+
+    public void OnDamageReceived(ProjectileProperties projectileProp, int newHP) {
+        if (newHP < 1) {
+            physic.enabled = false;
+            topBody.SetActive(false);
+            BottomBody.SetActive(false);
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/Enemies/Berserker/BerserkerTopAnimEvents.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class BerserkerTopAnimEvents : MonoBehaviour {
+
+    public Animator bottonAnim;
+
+    public void AEAttackEnd() {
+        gameObject.SetActive(false);
+        bottonAnim.SetBool("attack", false);
+    }
+        
+}
