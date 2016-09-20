6375eab54ef9923c04f29c33b54ba5d534341aa4
pinkas <benoit.pinkasfeld@gmail.com>
2016-09-20 19:33:32 +1000

    Berserker fully implemented (for now)

--- Assets/Scripts/Enemies/Berserker/Berserker.cs
+++ Assets/Scripts/Enemies/Berserker/Berserker.cs
@@ -4,21 +4,40 @@ using UnityEngine;
 public class Berserker : MonoBehaviour, IReceiveDamage {
 
     public GameObject topBody;
-    public GameObject BottomBody;
-    public Animator BottomBodyAnimator;
+    public GameObject bottomBody;
+    public GameObject bloodSplash;
+    public GameObject limbs;
+    private Animator bottomBodyAnimator;
     private SlugPhysics physic;
     private AreaOfEffectProjectile knife;
+    private Collider2D col;
+    private TimeUtils timeUtils;
+    public bool walkingMode = true;
 
     void Awake() {
         physic = GetComponent<SlugPhysics>();
         knife = GetComponent<AreaOfEffectProjectile>();
+        bottomBodyAnimator = bottomBody.GetComponent<Animator>();
+        col = GetComponent<Collider2D>();
+        timeUtils = GetComponent<TimeUtils>();
     }
 
-    public void OnEnable() {
+    void OnEnable() {
+        bottomBody.SetActive(true);
+        bloodSplash.SetActive(false);
+        limbs.SetActive(false);
+        RepositionLimbs();
+        col.enabled = true;
+        physic.enabled = true;
+        if (!walkingMode) {
+            bottomBodyAnimator.Play("berserker-sitting");
+        }
     }
 
     public void FixedUpdate() {
-        physic.MoveForward();
+        if (walkingMode) {
+            physic.MoveForward(35);
+        }
     }
 
     public void OnTriggerEnter2D(Collider2D collision) {
@@ -29,17 +48,35 @@ public class Berserker : MonoBehaviour, IReceiveDamage {
 
     private void Attack() {
         topBody.SetActive(true);
-        BottomBodyAnimator.SetBool("attack", true);
+        bottomBodyAnimator.SetBool("attack", true);
         knife.CastAOE("Player", transform.position); 
     }
 
     public void OnDamageReceived(ProjectileProperties projectileProp, int newHP) {
         if (newHP < 1) {
-            physic.enabled = false;
-            topBody.SetActive(false);
-            BottomBody.SetActive(false);
+            DesInit();
+            bloodSplash.SetActive(true);
+            limbs.SetActive(true);
         }
     }
 
+    public void SetWalkingMode() {
+        walkingMode = true;
+        bottomBodyAnimator.SetTrigger("walk");
+    }
+
+    private void DesInit() {
+        bottomBody.SetActive(false);
+        topBody.SetActive(false);
+        physic.enabled = false;
+        col.enabled = false;
+        timeUtils.TimeDelay(2, () => { gameObject.SetActive(false); });
+    }
+
+    private void RepositionLimbs() {
+        foreach (Transform bodyPart in limbs.transform) {
+            bodyPart.transform.position = limbs.transform.position;
+        }
+    }
 
 }
