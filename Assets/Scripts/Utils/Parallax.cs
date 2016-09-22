5c98d2754c7e0bc6de071c2d828e5a4eb19bf4e6
pinkas <benoit.pinkasfeld@gmail.com>
2016-09-22 19:06:55 +1000

    now using orthographic camera

---
+++ Assets/Scripts/Utils/Parallax.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public struct BgLayer {
+    public GameObject go;
+    public float scrollingRatioToForeground;
+}
+
+public class Parallax : MonoBehaviour {
+
+    public BgLayer[] bgLayers;
+    private Vector2 oldPos;
+
+    void Start() {
+        oldPos = transform.position;
+    }
+
+    void FixedUpdate () {
+        Vector2 camTrans = (Vector2) transform.position - oldPos;
+
+        if (camTrans != Vector2.zero) {
+            for (int i=0; i<bgLayers.Length; i++){
+                bgLayers[i].go.transform.Translate(
+                        (1-bgLayers[i].scrollingRatioToForeground)*camTrans);
+            }
+        }
+        oldPos = transform.position;
+	}
+}
