55d336ea5d5d21324f5d0222aeaa32fe686bfdda
pinkas <benoit.pinkasfeld@gmail.com>
2016-09-27 22:48:30 +1000

    add campfire

---
+++ Assets/Scripts/Enemies/Soldier/CampFire.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class CampFire : MonoBehaviour {
+
+    public GameObject soldier1;
+    public GameObject soldier2;
+    public EnemySpawner spawn1;
+    public EnemySpawner spawn2;
+    private BoxCollider2D boxCol;
+
+    void Awake() {
+        boxCol = GetComponent<BoxCollider2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if (col.tag == "Player") {
+            PlayerCrashesTheParty();
+            boxCol.enabled = false;
+        }
+
+    }
+   
+    private void PlayerCrashesTheParty() {
+        soldier1.SetActive(false);
+        soldier2.SetActive(false);
+        spawn1.enabled = true;
+        spawn2.enabled = true;
+        EventManager.TriggerEvent("player_back_alive");
+    }
+
+
+
+}
---
+++ Assets/Scripts/Enemies/Soldier/CampFireBlowingSoldier.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CampFireBlowingSoldier : MonoBehaviour {
+
+    public Animator fire;
+    public Animator otherSoldier;
+
+
+    public void AEBlow() {
+        float moreOrLessBlown = UnityEngine.Random.Range(0, 1f);
+        fire.SetFloat("blownIntensity", moreOrLessBlown);
+        fire.SetTrigger("blow");
+        if (moreOrLessBlown > 0.75f) {
+            otherSoldier.SetTrigger("hot");
+        }
+    }
+
+}
