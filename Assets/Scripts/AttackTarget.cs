b20590e99f1c6bc991959c503e38cdddde22ea20
pinkas <benoit.pinkasfeld@gmail.com>
2016-09-30 08:37:28 +1000

    attackTarget script now works with a signle go and not a pool

--- Assets/Scripts/AttackTarget.cs
+++ Assets/Scripts/AttackTarget.cs
@@ -3,15 +3,18 @@ using UnityEngine;
 
 public class AttackTarget : MonoBehaviour, IAttack {
 
-    public ObjectPoolScript grenadePool;
+    public GrenadeParabola grenade;
     public string victimsTag = "Player";
     private Animator animator;
     private Transform target;
 
     void Awake () {
         animator = GetComponent<Animator>();
-        target = GameObject.FindGameObjectWithTag("Player").transform;
 	}
+
+    void Start() {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
 
     public void Execute(string victimTag, Vector3 dir = default(Vector3), Vector3 ProjectileInitalPos = default(Vector3)) {
         animator.SetTrigger("grenade_standing");
@@ -19,10 +22,9 @@ public class AttackTarget : MonoBehaviour, IAttack {
 
     public void GrenadeAttack() {
         Vector2 projectileDestination = target.transform.position;
-        GameObject grenadeGameObject = grenadePool.GetPooledObject();
-        IProjectile grenade = grenadeGameObject.GetComponentInChildren<IProjectile>();
-        grenadeGameObject.transform.position = transform.position + new Vector3(0,0.2f);
-        grenadeGameObject.transform.right = transform.right;
+        grenade.transform.position = transform.position + new Vector3(0, 0.2f);
+        grenade.transform.right = transform.right;
+        grenade.gameObject.SetActive(true);
         grenade.Launch(victimsTag, projectileDestination);
     }
 }
