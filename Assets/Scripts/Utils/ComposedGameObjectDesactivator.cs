83b5f0cb586cfbbc1c79239267ed993ae9096f64
pinkas <benoit.pinkasfeld@gmail.com>
2016-09-30 08:10:12 +1000

    add gameObjectDesactivator

---
+++ Assets/Scripts/Utils/ComposedGameObjectDesactivator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ComposedGameObjectDesactivator : MonoBehaviour {
+
+    public GameObject[] objs;
+
+    void OnEnable() {
+        StartCoroutine("PeriodicCheck");
+    }
+
+    private IEnumerator PeriodicCheck() {
+        while (true) {
+            bool foundActiveObj = false;
+            for (int i = 0; i < objs.Length; i++) {
+                if (objs[i].activeSelf) {
+                    foundActiveObj = true;
+                    break;
+                }
+            }
+
+            if (!foundActiveObj) {
+                gameObject.SetActive(false);
+            }
+
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
