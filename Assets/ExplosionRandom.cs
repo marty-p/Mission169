6882e3bae17df4a5439c8b35b7d5bc9b9fc61468
pinkas <benoit.pinkasfeld@gmail.com>
2016-10-01 15:28:52 +1000

    explosion random initial commit

---
+++ Assets/ExplosionRandom.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplosionRandom : MonoBehaviour {
+
+    private Animator anim;
+    private float exploType;
+
+    void Awake() {
+        anim = GetComponent<Animator>();
+
+        exploType  = UnityEngine.Random.Range(0f, 1f);
+    }
+
+    public void OnEnable() {
+        anim.SetTrigger("explode");
+        anim.SetFloat("explosion_type", exploType);
+    }
+}
