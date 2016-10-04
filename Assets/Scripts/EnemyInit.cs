8c2350002ef4ba26cc67673aa1d633648e95babf
pinkas <benoit.pinkasfeld@gmail.com>
2016-10-05 09:01:10 +1100

    restore soldier's hp at death

--- Assets/Scripts/EnemyInit.cs
+++ Assets/Scripts/EnemyInit.cs
@@ -4,10 +4,16 @@ using Slug;
 public class EnemyInit : MonoBehaviour {
 
     public SlugLayers initLayer;
+    private HealthManager healthManager;
+
+    void Awake() {
+        healthManager = GetComponent<HealthManager>();
+    }
 
     void OnEnable() {
         gameObject.layer = (int) initLayer;
         transform.localPosition = Vector3.zero;
+        healthManager.currentHP = healthManager.maxHP;
     }
 
 }
