371dff96f2a724125ad2a838b6afcf5b3f0e1dba
pinkas <benoit.pinkasfeld@gmail.com>
2016-10-05 09:09:09 +1100

    debris manager and item manager initial check in

---
+++ Assets/DebrisManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class DebrisManager : MonoBehaviour {
+
+    private SlugPhysics physic;
+    private float velX;
+    private float velY;
+    public bool spin;
+    private float ZRotationFactorInitial = 10;
+    private Vector3 ZRotationStep;
+    private Blink blink;
+    public bool blinkAtTheEnd;
+    public bool fallOnly;
+    private bool blinking;
+    private SpriteRenderer spriteRenderer;
+
+    void Awake() {
+        physic = GetComponent<SlugPhysics>();
+        blink = GetComponent<Blink>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        velX = UnityEngine.Random.Range(-2.5f, 1.5f);
+        velY = UnityEngine.Random.Range(0.5f, 3f);
+        if (fallOnly) {
+            velY /= 10;
+        }
+    }
+
+    void OnEnable() {
+        blinking = false;
+        physic.enabled = true;
+        physic.SetVelocityX(velX);
+        physic.SetVelocityY(velY);
+        ZRotationStep.z =  ZRotationFactorInitial;
+        spriteRenderer.enabled = true;
+    }
+
+    void LateUpdate() {
+        if (Mathf.Approximately(physic.GetVelocity().y, 0)) {
+            Desactivate();
+        } else if (spin && ZRotationStep.z > 0) {
+            transform.Rotate(ZRotationStep);
+            ZRotationStep.z = Mathf.MoveTowards(ZRotationStep.z, 0, ZRotationFactorInitial * Time.deltaTime/1.5f);
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "World") {
+            Desactivate();
+        }
+    }
+
+    private void Desactivate() {
+        physic.enabled = false;
+        if (blinkAtTheEnd && !blinking) {
+            blinking = true;
+            blink.BlinkPlease(() => { gameObject.SetActive(false); });
+        } else if (!blinkAtTheEnd) {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/ItemManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class ItemManager : MonoBehaviour {
+
+    public MarcoAttackManager attackManager;
+    public FlashUsingMaterial flashBlue;
+    public SlugAudioManager audioManager;
+
+
+    public void OnTriggerEnter2D(Collider2D col) {
+        if (col.tag == "Collectible") {
+            CollectibleDef item = col.GetComponent<CollectibleDef>();
+            if (item!= null) {
+                PickUpItem(item);
+            }
+        }
+    }
+
+    private void PickUpItem(CollectibleDef item) {
+        if (item.attackID > 0) {
+            attackManager.SetAttack(item.attackID, item.animController);
+            attackManager.UpdateBulletCount(item.bulletCount);
+        }
+        flashBlue.FlashForXSecs(0.18f);
+        audioManager.PlaySoundByClip(item.weaponNameAudio);
+    }
+
+}
