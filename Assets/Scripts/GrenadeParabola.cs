e8fdd9638c870e3c846122b602311e1fdedf41c3
pinkas <benoit.pinkasfeld@gmail.com>
2016-10-05 08:59:51 +1100

    fix enemy grenade to not have its speed framerate dependant + using animator

--- Assets/Scripts/GrenadeParabola.cs
+++ Assets/Scripts/GrenadeParabola.cs
@@ -3,8 +3,8 @@
 public class GrenadeParabola : MonoBehaviour, IProjectile {
 
     public ProjectileProperties properties;
+    public Animator explosion;
 
-    private Vector3 ZRotationStep;
     private bool launched;
 
     private float x;
@@ -14,21 +14,20 @@ public class GrenadeParabola : MonoBehaviour, IProjectile {
     private float xIncrement;
     private float dir;
 
-
-
     void Update () {
         if (launched) {
             float height = 0.8f;
             // +height^2 is added to make sure that f(0) is 0 and not height^2
             float y = -(dist*x - height) * (dist*x - height) + height*height;
             transform.position = new Vector2(xOffset + x * dir, yOffset + y);
-            x += xIncrement;
+            x += xIncrement*Time.deltaTime;
         }
 	}
 
     public void Launch(string victimsTag, Vector2 destination) {
         launched = true;
         properties.victimTag = victimsTag;
+        transform.localPosition = Vector3.zero;
 
         x = 0;
         xOffset = transform.position.x;
@@ -36,7 +35,7 @@ public class GrenadeParabola : MonoBehaviour, IProjectile {
         dir = transform.right.x;
 
         float distWithTarget = Mathf.Abs(destination.x - transform.position.x);
-        xIncrement = (distWithTarget/1.3f)*Time.deltaTime;
+        xIncrement = (distWithTarget/1.2f);
         // 1.5f est trouve de maniere empirique. Plus dir est grand plus la parabole est petite
         // just a way to get the grenade to land aproximately where the player is
         dist = 1.5f *( 1 / distWithTarget);
@@ -51,13 +50,12 @@ public class GrenadeParabola : MonoBehaviour, IProjectile {
     }
 
     private void Explode(Collider2D col) {
-        ProjectileUtils.ImpactAnimationAndSound(transform, col, properties);
+        explosion.gameObject.SetActive(true);
+        explosion.transform.position = this.transform.position;
+        explosion.Play("1");
+        //ProjectileUtils.ImpactAnimationAndSound(transform, col, properties);
         ProjectileUtils.NotifyCollider(col, properties);
         gameObject.SetActive(false);
     }
 
-    void OnBecameInvisible() {
-        gameObject.SetActive(false);
-        launched = false;
-    }
 }
