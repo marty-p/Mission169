5b7dd0eb8306f614580235e268d28930499d7e22
pinkas <benoit.pinkasfeld@gmail.com>
2016-10-16 10:58:43 +1100

    infiniteScrolling fix alignment bug

--- Assets/Scripts/Utils/InfiiniteScrolling.cs
+++ Assets/Scripts/Utils/InfiiniteScrolling.cs
@@ -3,15 +3,17 @@ using System.Collections;
 
 public class InfiiniteScrolling : MonoBehaviour {
 
+    //TODO have an array of 'bits', would come handy if scrolling more than 2 sprites
     public SpriteRenderer bit1;
     public SpriteRenderer bit2;
     public Camera cam;
     public float scrollingFact = 0.025f;
 
 	void Update () {
-        bit1.transform.transform.Translate(-Time.deltaTime * scrollingFact, 0, 0);
-        bit2.transform.transform.Translate(-Time.deltaTime * scrollingFact, 0, 0);
+        float transX = -Time.deltaTime * scrollingFact;
+        transform.transform.Translate(transX, 0, 0);
 
+        // went off screen, the sprite is positioned after the other one
         if (!bit1.isVisible) {
             bit1.transform.position = bit2.bounds.center + new Vector3(bit2.bounds.size.x, 0, 0);
         }
@@ -20,8 +22,8 @@ public class InfiiniteScrolling : MonoBehaviour {
         }
 
         if (!bit1.isVisible && !bit2.isVisible) {
-            bit1.transform.position = new Vector3(cam.transform.position.x, bit1.transform.position.y, 0);
             bit2.transform.position = bit1.bounds.center + new Vector3(bit1.bounds.size.x, 0, 0);
+            bit1.transform.position = new Vector3(cam.transform.position.x, bit1.transform.position.y, 0);
         }
 
 	}
