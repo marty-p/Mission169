f4ae2f76c12e308f037d50af155b91b20738c95a
pinkas <benoit.pinkasfeld@gmail.com>
2016-10-17 09:16:56 +1100

    game manager improved

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -15,59 +15,48 @@ namespace Mission169 {
         private bool bossStarted;
         private readonly float waitTimeBeforeSpawn = 1.5f;
 
-        private HUDManager hudManager;
-        private GameObject mainMenu;
         private TimeUtils timeUtils;
         private SlugAudioManager soundManager;
-        public Transform missionStartLocation;
+        private Transform missionStartPos;
         private AudioSource bgMusic;
         private GameObject playerGameObject;
         private PlayerDeathManager playerDeathManager;
-        public GameObject PlayerPrefab; // don't want to use Resources.Load for playa
-        public EnemySpawner endEnemySpwaner;
-        private HealthManager endEnemyHealth;
+        private HUDManager hud;
+        private MainMenu mainMenu;
 
         void Awake() {
             DontDestroyOnLoad(this);
 
-            timeUtils = GetComponent<TimeUtils>();
-            EventManager.StartListening("player_death", OnplayerDeath);
-            EventManager.StartListening("boss_start", BossStart);
-            EventManager.StartListening("mission_end", OnMissionEnd);
-            EventManager.StartListening("add_points", UpdatePlayerPoints);
+            timeUtils = gameObject.AddComponent<TimeUtils>();
+            EventManager.Instance.StartListening("player_death", OnplayerDeath);
+            EventManager.Instance.StartListening("boss_start", BossStart);
+            EventManager.Instance.StartListening("mission_end", OnMissionSuccess);
+            EventManager.Instance.StartListening("add_points", UpdatePlayerPoints);
             soundManager = GetComponentInChildren<SlugAudioManager>();
             // Player
-            playerGameObject = Instantiate(PlayerPrefab);
+            playerGameObject = Instantiate(Resources.Load("Marco")) as GameObject;
             playerDeathManager = playerGameObject.GetComponentInChildren<PlayerDeathManager>();
             playerGameObject.SetActive(false);
-            // UI
-            GameObject hudManagerGO = Instantiate(Resources.Load("HUD")) as GameObject;
-            hudManager = hudManagerGO.GetComponent<HUDManager>();
-            hudManager.SetVisible(false);
-            mainMenu = Instantiate(Resources.Load("MainMenu")) as GameObject;
-        }
-
-        void Start() {
-            endEnemyHealth = endEnemySpwaner.GetEnemy().GetComponent<HealthManager>();
-            OnMissionLoaded();
-        }
-
-        void Update() {
-            if (Input.GetButtonDown("Pause")) {
-                TogglePauseGame();
-            }
+            playerGameObject.transform.parent = transform;
+            hud = UIManager.Instance.HUD;
+            mainMenu = UIManager.Instance.MainMenuT;
         }
 
         public GameObject GetPlayer() {
             return playerGameObject;
         }
 
+        public void InitMission(Transform missionStartPos) {
+            this.missionStartPos = missionStartPos;
+        }
+
         public void MissionStart() {
             playerScore = 0; // TODO only one mission for now so ...
             playerLifeCount = playerLifeStart;
-            hudManager.SetLifeCount(playerLifeCount);
-            hudManager.SetVisible(true);
-            mainMenu.SetActive(false);
+            playerGameObject.transform.position = missionStartPos.position;
+            hud.SetLifeCount(playerLifeCount);
+            hud.SetVisible(true);
+            mainMenu.SetVisible(false);
             playerGameObject.SetActive(true);
             bgMusic = soundManager.PlaySound(0);
         }
@@ -77,22 +66,26 @@ namespace Mission169 {
             Time.timeScale = Time.timeScale == 0 ? 1 : 0;
         }
 
+        public void ShowMenu() {
+            mainMenu.SetVisible(true);
+        }
+
+        private void UpdatePlayerPoints(float pts) {
+            playerScore = playerScore + (int)pts;
+            hud.SetScore(playerScore);
+        }
+
         private void OnplayerDeath() {
             playerLifeCount--;
-            hudManager.SetLifeCount(playerLifeCount);
+            hud.SetLifeCount(playerLifeCount);
             if (playerLifeCount >= 0) {
                 timeUtils.TimeDelay(waitTimeBeforeSpawn, () => {
                     playerDeathManager.SpawnPlayer();
-                    EventManager.TriggerEvent("player_back_alive");
+                    EventManager.Instance.TriggerEvent("player_back_alive");
                 });
             } else {
                 GameOver();
             }
-        }
-
-        private void OnMissionLoaded() {
-            playerGameObject.transform.position = missionStartLocation.position;
-            playerGameObject.SetActive(false);
         }
 
         private void BossStart() {
@@ -101,28 +94,28 @@ namespace Mission169 {
             bgMusic = soundManager.PlaySound(1);
         }
 
-        private void OnMissionEnd() {
+        private void OnMissionSuccess() {
+            EventManager.Instance.TriggerEvent("mission_success");
             missionEnded = true;
             soundManager.PlaySound(2);
             soundManager.PlaySound(3);
             bgMusic.Stop();
-            playerGameObject.GetComponent<MovementManager>().StopMoving();
-            playerGameObject.GetComponent<InputManager>().enabled = false;
-            playerGameObject.GetComponent<AnimationManager>().MissionCompleteAnim();
+            playerGameObject.GetComponentInChildren<MovementManager>().StopMoving();
+            playerGameObject.GetComponentInChildren<InputManager>().enabled = false;
+            playerGameObject.GetComponentInChildren<AnimationManager>().MissionCompleteAnim();
             playerGameObject.layer = (int)SlugLayers.IgnoreRaycast; //to ignore any potential projectile still going
-
-            timeUtils.TimeDelay(10, () => { SceneManager.LoadScene("mainscene"); });
+            MissionEnd();
         }
 
         private void GameOver() {
             bgMusic.Stop();
             bgMusic = soundManager.PlaySound(4);
-            timeUtils.TimeDelay(5, () => { SceneManager.LoadScene("mainscene"); });
+            MissionEnd();
         }
 
-        private void UpdatePlayerPoints(float pts) {
-            playerScore = playerScore + (int)pts;
-            hudManager.SetScore(playerScore);
+        private void MissionEnd() {
+            AchievementManager.Instance.SaveAchievementsLocally();
+            timeUtils.TimeDelay(10, () => { SceneManager.LoadScene("mainscene"); });
         }
 
     }
