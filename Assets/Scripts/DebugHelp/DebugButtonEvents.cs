9915a02d3244a876e1bf9dc4ba103be54eaddba0
pinkas <benoit.pinkasfeld@gmail.com>
2016-10-17 23:26:38 +1100

    add debugbbuttonevent

---
+++ Assets/Scripts/DebugHelp/DebugButtonEvents.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using Mission169;
+
+public class DebugButtonEvents : MonoBehaviour {
+
+    void Awake() {
+        DontDestroyOnLoad(this);
+    }
+
+    void OnGUI() {
+        if (GUI.Button(new Rect(10, 10, 100, 40), "mission_success"))
+            EventManager.Instance.TriggerEvent("mission_success");
+
+        if (GUI.Button(new Rect(10, 70, 50, 30), "Click"))
+            EventManager.Instance.TriggerEvent("mission_success");
+
+    }
+
+
+}
