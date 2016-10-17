110fa9ea3093ea4d19130abc0a27889b668aaf15
pinkas <benoit.pinkasfeld@gmail.com>
2016-10-18 09:04:52 +1100

    GameManager now multi level proof

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,22 @@
 ﻿using UnityEngine;
 using Slug;
 using UnityEngine.SceneManagement;
+using SlugLib;
 
 namespace Mission169 {
 
     public class GameManager : Singleton<GameManager> {
 
-        protected GameManager() {}
+        protected GameManager() { }
 
-        private readonly int playerLifeStart = 2;
-        private int playerLifeCount;
+        private static readonly int playerLifeStart = 2;
+        private static int playerLifeCount = playerLifeStart;
         private int playerScore = 0;
         private bool missionEnded;
         private bool bossStarted;
         private readonly float waitTimeBeforeSpawn = 1.5f;
+        private int currentMissionID = 0;
+        private string[] missionList = new[] { "mission1", "mission2" };
 
         private TimeUtils timeUtils;
         private SlugAudioManager soundManager;
@@ -48,19 +51,22 @@ namespace Mission169 {
             return playerGameObject;
         }
 
-        public void InitMission(Transform missionStartPos) {
-            this.missionStartPos = missionStartPos;
+        public void MissionInit() {
+            MissionDef missionDef = FindObjectOfType(typeof(MissionDef)) as MissionDef; ;
+            playerGameObject.GetComponentInChildren<AnimationManager>().ResetAnimators();
+            playerGameObject.layer = (int)SlugLayers.Player; //to ignore any potential projectile still going
+            playerGameObject.transform.GetChild(0).transform.position = missionDef.startPos.position;
+            playerGameObject.SetActive(false);
+            //Init music too
         }
 
         public void MissionStart() {
-            playerScore = 0; // TODO only one mission for now so ...
-            playerLifeCount = playerLifeStart;
-            playerGameObject.transform.position = missionStartPos.position;
+            playerGameObject.GetComponentInChildren<InputManager>().enabled = true;
             hud.SetLifeCount(playerLifeCount);
             hud.SetVisible(true);
             mainMenu.SetVisible(false);
             playerGameObject.SetActive(true);
-            bgMusic = soundManager.PlaySound(0);
+            //bgMusic = soundManager.PlaySound(0);
         }
 
         //TODO block user input too
@@ -83,41 +89,67 @@ namespace Mission169 {
             if (playerLifeCount >= 0) {
                 timeUtils.TimeDelay(waitTimeBeforeSpawn, () => {
                     playerDeathManager.SpawnPlayer();
-                    EventManager.Instance.TriggerEvent("player_back_alive");
+                    EventManager.Instance.TriggerEvent(GlobalEvents.PlayerSpawned);
                 });
             } else {
                 GameOver();
             }
         }
 
-        private void BossStart() {
+        private void OnBossStart() {
             bossStarted = true;
             bgMusic.Stop();
             bgMusic = soundManager.PlaySound(1);
         }
 
         private void OnMissionSuccess() {
-            EventManager.Instance.TriggerEvent("mission_success");
             missionEnded = true;
-            soundManager.PlaySound(2);
-            soundManager.PlaySound(3);
-            bgMusic.Stop();
+            //soundManager.PlaySound(2);
+            //soundManager.PlaySound(3);
+            //bgMusic.Stop(); 
+            //TODO These should be in one function in one component of the player
             playerGameObject.GetComponentInChildren<MovementManager>().StopMoving();
             playerGameObject.GetComponentInChildren<InputManager>().enabled = false;
             playerGameObject.GetComponentInChildren<AnimationManager>().MissionCompleteAnim();
             playerGameObject.layer = (int)SlugLayers.IgnoreRaycast; //to ignore any potential projectile still going
             MissionEnd();
+            currentMissionID++;
+            if (currentMissionID < missionList.Length) {
+                //TODO BEN create a dialog that offers to go the next level
+                //UIManager.Instance.CreateSuccessDialog()
+                MissionLoad();
+                MissionInit();
+                Invoke("MissionStart", 5);
+            } else {
+                //UIManager.Instance.CreateGameBeatenDialogSuperSuccess()
+                currentMissionID = 0;
+                MissionLoad();
+                mainMenu.SetVisible(true);
+            }
+        }
+
+        private void MissionLoad() {
+            SceneManager.LoadScene(missionList[currentMissionID]);
+            SceneManager.LoadScene("loading", LoadSceneMode.Additive);
+            timeUtils.TimeDelay(2, () => SceneManager.UnloadScene("loading"));
         }
 
         private void GameOver() {
-            bgMusic.Stop();
-            bgMusic = soundManager.PlaySound(4);
+            //bgMusic.Stop();
+            //bgMusic = soundManager.PlaySound(4);
             MissionEnd();
+            playerScore = 0;
+            currentMissionID = 0;
+            playerLifeCount = playerLifeStart;
+            //TODO BEN create a dialog that offers to retry or go to the main menu
+            //UIManager.Instance.CreateGameOverDialog()
         }
 
         private void MissionEnd() {
             AchievementManager.Instance.SaveAchievementsLocally();
-            timeUtils.TimeDelay(10, () => { SceneManager.LoadScene("mainscene"); });
+        }
+
+        void OnMissionLoaded(Scene scene, LoadSceneMode mode) {
         }
 
     }
