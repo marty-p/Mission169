35d75074bf04c0ff7ee5679d777d274367233a13
pinkas <benoit.pinkasfeld@gmail.com>
2016-10-18 20:47:02 +1100

    each mission now takes care of its music

---
+++ Assets/Scripts/SceneAudio.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using SlugLib;
+
+namespace Mission169 {
+
+    public class SceneAudio : MonoBehaviour {
+
+        private SlugAudioManager soundManager;
+        private AudioSource music;
+
+        void Awake() {
+            soundManager = GetComponent<SlugAudioManager>();
+            EventManager.Instance.StartListening(GlobalEvents.MissionStart, PlayMainMusic);
+            EventManager.Instance.StartListening(GlobalEvents.BossStart, PlayBossMusic);
+            EventManager.Instance.StartListening(GlobalEvents.BossDead, PlaySuccess);
+            EventManager.Instance.StartListening(GlobalEvents.GameOver, PlayGameOver);
+        }
+
+        void PlayMainMusic() {
+            music = soundManager.PlaySound(0);
+        }
+
+        void PlayBossMusic() {
+            music.Stop();
+            music = soundManager.PlaySound(1);
+        }
+
+        void PlaySuccess() {
+            music.Stop();
+            soundManager.PlaySound(2);
+            soundManager.PlaySound(3);
+        }
+
+        void PlayGameOver() {
+            music.Stop();
+            soundManager.PlaySound(4);
+        }
+    }
+
+}
