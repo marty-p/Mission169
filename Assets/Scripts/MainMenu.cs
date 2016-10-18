2d8415f094c878bcda58ac7aaf9a616467fa6403
pinkas <benoit.pinkasfeld@gmail.com>
2016-10-18 21:07:37 +1100

    mainMenu, enable facebook button

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,9 +6,16 @@ public class MainMenu : MonoBehaviour {
     public Button start;
     public Button museum;
     public Button thanks;
+
+    public Button facebook;
     
     void Start() {
         start.onClick.AddListener(GameManager.Instance.MissionStart);
+        facebook.onClick.AddListener(FacebookManager.Instance.ShareLink);
     }    
+
+    public void SetVisible(bool visible) {
+        gameObject.SetActive(visible);
+    }
 
 }
