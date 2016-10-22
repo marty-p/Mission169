77947b4c5a43e6f6eac1c0d9e6504d360a91d0ab
Benoit Pinkasfeld <benoitpinkasfeld@Benoits-iMac.local>
2016-10-22 13:05:50 +1100

    lots of work on achievements and achievement system

---
+++ Assets/Scripts/Achievement/IGameService.cs
@@ -0,0 +1,6 @@
+using System;
+
+public interface IGameService {
+    void UpdateAchievement(Achievement Achievement);
+    void Reset();
+} 
---
+++ Assets/Scripts/Achievement/SlugGooglePlayGames.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class SlugGooglePlayGames : MonoBehaviour, IGameService {
+
+    void Start () {
+    }
+	public void UpdateAchievement(Achievement achievement){
+
+	}
+	public void Reset(){
+	}
+
+}
