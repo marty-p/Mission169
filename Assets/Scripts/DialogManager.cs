fbbac5f225b4152a2bd6dccb3fb74c0e6c99228f
pinkas <benoit.pinkasfeld@gmail.com>
2016-10-23 23:41:07 +1100

    add ugly DialogManager.cs should have been with 92aa7ed91beeb4d56c8498a1609bf2df284d524c ...

---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Mission169;
+
+public class DialogManager : MonoBehaviour {
+    private GameObject currentButtonGroup;
+    private Animator transitionAnimator;
+
+    public SlugReplay replay;
+
+    public Button pauseResumeButton;
+    public Button pauseHomeButton;
+    public Button pauseRetryButton;
+    public Button nextLevel;
+
+    public GameObject pauseButtons;
+    public GameObject successButtons;
+    public GameObject gameOverButtons;
+
+    void Awake() {
+        pauseResumeButton.onClick.AddListener(OnResumePressed);
+        pauseHomeButton.onClick.AddListener(OnHomePressed);
+        pauseRetryButton.onClick.AddListener(OnRetryPressed);
+        nextLevel.onClick.AddListener(OnNextPressed);
+        transitionAnimator = GetComponent<Animator>();
+    }
+
+    public void OnResumePressed() {
+        GameManager.Instance.PauseGame(false);
+        Transition(false);
+    }
+
+    public void OnHomePressed() {
+        GameManager.Instance.Home();
+        Transition(false);
+    }
+
+    public void OnRetryPressed() {
+        GameManager.Instance.MissionRetry();
+        GameManager.Instance.PauseGame(false);
+        Transition(false);
+    }
+
+    public void OnNextPressed() {
+        GameManager.Instance.GoNextMission();
+        GameManager.Instance.PauseGame(false);
+        Transition(false);
+    }
+
+    public void SetVisible(bool visible) {
+        //currentButtonGroup.SetActive(false);
+        Transition(visible);
+    }
+
+    public void Activate(DialogType dialogType) {
+        gameObject.SetActive(true);
+        if (replay != null) {
+            replay.gameObject.SetActive(true);
+        }
+
+        if (dialogType == DialogType.Pause) {
+            currentButtonGroup = pauseButtons;
+        } else if(dialogType == DialogType.MissionSuccess) {
+            currentButtonGroup = successButtons;
+        } else if (dialogType == DialogType.GameOver) {
+            currentButtonGroup = gameOverButtons;
+        }
+        currentButtonGroup.SetActive(true);
+        SetVisible(true);
+    }
+
+    public void AEtransitionInDone() {
+        GameManager.Instance.PauseGame(true);
+    }
+
+    public void AEtransitionOutDone() {
+        SetVisible(false);
+        gameObject.SetActive(false);
+    }
+
+    void Transition(bool on) {
+        transitionAnimator.ResetTrigger("on_screen");
+        transitionAnimator.ResetTrigger("off_screen");
+        string trigger = on ? "on_screen" : "off_screen";
+        transitionAnimator.SetTrigger(trigger);
+    }
+
+}
+
+public enum DialogType {
+    Pause,
+    GameOver,
+    MissionSuccess
+}
