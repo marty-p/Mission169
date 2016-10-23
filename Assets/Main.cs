4e17e0cdad69a85a86f0d90b6489c07ffe4a3d9a
pinkas <benoit.pinkasfeld@gmail.com>
2016-10-24 00:00:03 +1100

    main now instantiate uimanager from prefab .

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -7,15 +7,14 @@ public class Main : MonoBehaviour {
 
     public GameObject achievementManagerPrefab;
     public GameObject gameManagerPrefab;
-    private readonly string firstMission = "mission2";
+    public GameObject uiManagerPrefab;
+    public string firstMission = "mission1";
 
 	void Awake () {
         SceneManager.sceneLoaded += OnSceneLoaded;
-        SceneManager.LoadScene("loading", LoadSceneMode.Additive);
-        
         // Creating singletons (in the right order is better)
         EventManager eventManager = EventManager.Instance;
-        UIManager uiManager = UIManager.Instance;
+        GameObject uiManager = Instantiate(uiManagerPrefab);
         GameObject achievementManagerSingleton = Instantiate(achievementManagerPrefab);
         GameObject gameManager = Instantiate(gameManagerPrefab);
 #if UNITY_IOS || UNITY_ANDROID
@@ -26,9 +25,7 @@ public class Main : MonoBehaviour {
 	
     void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
         if (scene.name == firstMission) {
-            SceneManager.UnloadScene("loading");
-            GameManager.Instance.MissionInit();
-            UIManager.Instance.MainMenuT.SetVisible(true);
+            GameManager.Instance.Home();
             SceneManager.sceneLoaded -= OnSceneLoaded;
         }
     }
