dad9b12428fbf67cae397474baa0938713e1f5c5
pinkas <benoit.pinkasfeld@gmail.com>
2016-10-25 06:26:51 +1100

    add script to disable pixel perfect depending on aspect ratio (mainly for ipad)

---
+++ Assets/Scripts/SlugPixelPerfect.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class SlugPixelPerfect : MonoBehaviour {
+
+	void Start () {
+        Resolution res = Screen.currentResolution;
+        float aspectRatio = (float)res.width / res.height;
+        if (aspectRatio < 1.5f) {
+            GetComponent<PixelPerfectCamera>().pixelPerfect = false;
+        }
+    }
+	
+}
