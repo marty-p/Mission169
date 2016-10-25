a900873218bebd25ca9bab47337f9c0b247f3938
pinkas <benoit.pinkasfeld@gmail.com>
2016-10-25 20:30:36 +1100

    change buttons actions main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,8 +4,8 @@ using Mission169;
 
 public class MainMenu : MonoBehaviour {
     public Button start;
-    public Button museum;
-    public Button thanks;
+    public Button reset;
+    public Button git;
 
     public Button facebook;
     public Button gameCenter;
@@ -24,6 +24,8 @@ public class MainMenu : MonoBehaviour {
         start.onClick.AddListener(GameManager.Instance.MissionStart);
         facebook.onClick.AddListener(OnFacebookPressed);
         gameCenter.onClick.AddListener(GameCenterManager.ShowAchievements);
+        git.onClick.AddListener(OnGitHubPressed);
+        reset.onClick.AddListener(AchievementManager.Instance.ResetAchievements);
     }    
 
     public void SetVisible(bool visible) {
@@ -37,4 +39,7 @@ public class MainMenu : MonoBehaviour {
         Application.OpenURL("https://m.facebook.com/benoitpinkasfeld");
     }
 
+    private void OnGitHubPressed() {
+        Application.OpenURL("https://github.com/pinkas");
+    }
 }
