cbf42a39b747084ce1c46990d2b79bb16ed7598a
pinkas <benoit.pinkasfeld@gmail.com>
2016-10-30 19:38:18 +1100

    rework on soldier prefab

---
+++ Assets/Scripts/Enemies/EnemyBuilder.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using Slug;
+using SlugLib;
+
+public class EnemyBuilder : MonoBehaviour, IReceiveDamage {
+
+    public int maxHP = 1;
+    public SlugLayers initLayer;
+    public int pointsValue = 100;
+    private HealthManager healthManager;
+    public AudioClip[] audioClips;
+
+
+    void Awake() {
+        healthManager = gameObject.AddComponent<HealthManager>();
+        SlugAudioManager audioManager = gameObject.AddComponent<SlugAudioManager>();
+        audioManager.InitAudioClips(audioClips);
+        gameObject.AddComponent<TimeUtils>();
+        gameObject.AddComponent<Blink>();
+    }
+
+
+    void OnEnable() {
+        gameObject.layer = (int)initLayer;
+        transform.localPosition = Vector3.zero;
+        healthManager.currentHP = maxHP;
+    } 
+
+    public void OnDamageReceived(ProjectileProperties projectileProp, int newHP) {
+        if (newHP > 0) {
+            return;
+        } else {
+            //To ignore collision with projectiles during death anim but still be 'physic'
+            gameObject.layer = 2;
+            EventManager.Instance.TriggerEvent(GlobalEvents.PointsEarned, pointsValue);
+            EventManager.Instance.TriggerEvent(GlobalEvents.SoldierDead);
+        }
+    }
+
+
+}
