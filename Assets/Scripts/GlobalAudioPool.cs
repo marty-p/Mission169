9c23e6b25202a4f5251533508f573bcd18a20989
pinkas <benoit.pinkasfeld@gmail.com>
2016-10-30 19:40:12 +1100

    add new global audio pool(so that not all gameobjects has its own pool of audiosources)

---
+++ Assets/Scripts/GlobalAudioPool.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class GlobalAudioPool : Singleton<GlobalAudioPool> {
+
+    public GameObject soundPrefab;
+    private ObjectPoolScript audioSourcePool;
+
+	void Awake () {
+        DontDestroyOnLoad(this);
+
+	    audioSourcePool = gameObject.AddComponent<ObjectPoolScript>();
+        audioSourcePool.pooledObject = soundPrefab;
+        audioSourcePool.pooledAmount = 10;
+        audioSourcePool.willGrow = true;
+	}
+
+    public AudioSource GetAudioSource() {
+        return audioSourcePool.GetPooledObject().GetComponent<AudioSource>();
+    }	
+
+}
