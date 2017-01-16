56fd07ae7a1817632d61dd813ed59125b163c081
pinkas <benoit.pinkasfeld@gmail.com>
2017-01-16 20:33:57 +1100

    update to main.cs

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -2,30 +2,46 @@
 using UnityEngine.SceneManagement;
 using Mission169;
 using SlugLib;
+using System.Collections;
 
 public class Main : MonoBehaviour {
 
     public GameObject achievementManagerPrefab;
     public GameObject gameManagerPrefab;
     public GameObject uiManagerPrefab;
+    public GameObject globalAudioPoolPrefab;
     public string firstMission = "mission1";
+    private TimeUtils timeUtils;
 
 	void Awake () {
-        SceneManager.sceneLoaded += OnSceneLoaded;
-        // Creating singletons (in the right order is better)
-        EventManager eventManager = EventManager.Instance;
-        GameObject uiManager = Instantiate(uiManagerPrefab);
-        GameObject achievementManagerSingleton = Instantiate(achievementManagerPrefab);
-        GameObject gameManager = Instantiate(gameManagerPrefab);
-#if UNITY_IOS || UNITY_ANDROID
-        FacebookManager facebook = FacebookManager.Instance;
-#endif
+        timeUtils = GetComponent<TimeUtils>();
+
 #if UNITY_IOS
         Application.targetFrameRate = 60;
 #endif
-        SceneManager.LoadScene(firstMission);
+
+        // Creating singletons (in the right order is better)
+        EventManager eventManager = EventManager.Instance;
+        Instantiate(uiManagerPrefab);
+        Instantiate(achievementManagerPrefab);
+        Instantiate(gameManagerPrefab);
+        Instantiate(globalAudioPoolPrefab);
+        FacebookManager facebookManager = FacebookManager.Instance;
+
+        UIManager.Instance.blackOverlay.SetVisible(true);
+
+        FacebookManager.Instance.OnFacebookInitSuccess = () => {
+        //SceneManager.LoadScene("login", LoadSceneMode.Additive);
+            // Facebook init is done we can load the scene in the background 
+            SceneManager.LoadScene(firstMission, LoadSceneMode.Additive);
+
+            timeUtils.TimeDelay(1, () => {
+                GameManager.Instance.Home();
+                UIManager.Instance.blackOverlay.FadeOut();
+            });
+        };
 	}
-	
+
     void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
         if (scene.name == firstMission) {
             GameManager.Instance.Home();
