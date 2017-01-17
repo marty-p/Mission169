84a40e3e1d2fd2a07ca79af356258af7a2a188c7
pinkas <benoit.pinkasfeld@gmail.com>
2017-01-17 22:16:16 +1100

    fix

---
+++ Assets/Scripts/TransitionOverlay.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Mission169;
+
+public class TransitionOverlay : MonoBehaviour {
+
+    private Image blackOverlay;
+
+	// Use this for initialization
+	void Awake () {
+        blackOverlay = GetComponent<Image>();
+	}
+
+    public void SetVisible(bool visible) {
+        gameObject.SetActive(visible);
+    }
+
+    public void FadeOut() {
+        blackOverlay.CrossFadeAlpha(0, 0.15f, false);
+    }
+
+    public void FadeIn() {
+        blackOverlay.CrossFadeAlpha(255, 0.15f, false);
+    }
+	
+}
